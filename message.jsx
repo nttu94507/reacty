f5635c099851b3c22108683c78ef67cebad17701
nttu94507 <nttu94507@gmail.com>
2020-04-21 21:39:37 +0800

    redux get store

--- message.jsx
+++ message.jsx
@@ -1,5 +1,10 @@
 import React from "react"
 import ReactDOM from "react-dom"
+//從react-redux匯入Provider組件
+import {Provider} from "react-redux"
+//從index.js中匯出store，如果大大的路徑和我不同，記得要改一下！
+import store from "./index.js"
+import {connect} from "react-redux"
 
 class MessageList extends React.Component {
     render(){
@@ -17,8 +22,23 @@ class MessageList extends React.Component {
 
 class MessageForm extends React.Component {
     render(){
-        return <MessageList />
+        return(
+            //使用Provider組件記得要透過store屬性傳入import進來的store資料
+            <Provider store={store}>
+                //原本的MessageList已經被connect包成List了
+                <List />
+            </Provider>
+        )
     }
 }
 
-ReactDOM.render(<MessageForm/>,document.getElementById('root'))
+//上方的data會被傳入這個function中的state
+const mapStateToProps = state => {
+    /*透過回傳的物件來指定要取的資料，
+    這裡把message取走，然後key值用data回傳*/
+    return { data: state.message }
+}
+
+const List = connect(mapStateToProps)(MessageList)
+
+ReactDOM.render(<MessageForm/>,document.getElementById('root1'))
