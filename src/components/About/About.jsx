0456c9fd1f5aa7efce8b45d4596253c2205d819d
nttu94507 <nttu94507@gmail.com>
2020-05-04 18:25:28 +0800

    redirect seccess

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, Route, Switch } from "react-router-dom"
+import { Link, Route, Switch, Redirect } from "react-router-dom"
 
 class About extends React.Component{
     render(){
@@ -14,8 +14,8 @@ class About extends React.Component{
             </ul>
             <Switch>
                 <Route exact path={`${this.props.match.path}/introd`} component={Introd} />
-                <Route exact path={`${this.props.match.path}/his`} component={His} />
-                <Route exact path={`${this.props.match.path}/story`} component={Nopage} />
+                <Route  path={`${this.props.match.path}/his`} component={His} />
+                <Redirect from={`${this.props.match.path}/story`} to={{pathname:`${this.props.match.path}/his`,search: "?hey=uccu",state:{name:'Referrer'}}}/>
             </Switch>
             
         </div>
@@ -31,7 +31,7 @@ class Aboutcontent extends React.Component{
         if (this.props.match.path == "introd")
             content = "這裡是理念介紹"
 
-        else if ('this.props.match.path == "his"')
+        else if (this.props.match.path == "his")
             content = "這裡是歷史沿革" 
         else
             content = "頁面維護中..."   
@@ -48,6 +48,7 @@ class Introd extends React.Component {
 
 class His extends React.Component {
     render() {
+        console.log(this.props.location.state)
         return <p>這裡是歷史沿革</p>
     }
 }
