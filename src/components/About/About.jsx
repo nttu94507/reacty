79a6a0ebb7f80331c16a3bed98e7de6ac271b399
nttu94507 <nttu94507@gmail.com>
2020-05-04 17:09:06 +0800

    switch route seccess

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,5 +1,5 @@
 import React from "react"
-import { Link, Route } from "react-router-dom"
+import { Link, Route, Switch } from "react-router-dom"
 
 class About extends React.Component{
     render(){
@@ -10,8 +10,14 @@ class About extends React.Component{
             <ul>
                 <li><Link to={`${this.props.match.url}/introd`}>理念介紹</Link></li>
                 <li><Link to={`${this.props.match.url}/his`}>歷史沿革</Link></li>
+                <li><Link to={`${this.props.match.url}/story`}>品牌故事</Link></li>
             </ul>
-            <Route exact path={`${this.props.match.path}/:type`} component={Aboutcontent} /> 
+            <Switch>
+                <Route exact path={`${this.props.match.path}/introd`} component={Introd} />
+                <Route exact path={`${this.props.match.path}/his`} component={His} />
+                <Route exact path={`${this.props.match.path}/story`} component={Nopage} />
+            </Switch>
+            
         </div>
         )
     }
@@ -22,12 +28,33 @@ class Aboutcontent extends React.Component{
     render(){
         console.log(this.props.match)
         let content
-        if (this.props.match.params.type == "introd")
+        if (this.props.match.path == "introd")
             content = "這裡是理念介紹"
+
+        else if ('this.props.match.path == "his"')
+            content = "這裡是歷史沿革" 
         else
-            content = "這裡是歷史沿革"   
+            content = "頁面維護中..."   
         
         return(<p>{content}</p>)
+    }
+}
+
+class Introd extends React.Component {
+    render() {
+        return <p>這裡是理念介紹</p>
+    }
+}
+
+class His extends React.Component {
+    render() {
+        return <p>這裡是歷史沿革</p>
+    }
+}
+
+class Nopage extends React.Component{
+    render() {
+        return <p>頁面維護中...</p>
     }
 }
 
